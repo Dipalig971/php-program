39e1ebe9cfcf430b749a4090f7465869b0785688
Dipali gunjal <143181151+Dipalig971@users.noreply.github.com>
2024-11-22 13:41:40 +0530

    Add files via upload

---
+++ php program/config/config.php
@@ -0,0 +1,47 @@
+<?php
+
+class Config
+{
+    private $localhost = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "student";
+    private $status;
+
+    public function connectDatabase()
+    {
+        $this->status = mysqli_connect($this->localhost, $this->username, $this->password, $this->database, );
+    }
+
+    public function insertDatabase($name, $age, $phone) {
+       
+        $query = "INSERT INTO product (name, age, phone) VALUES ('$name', '$age', $phone)";
+        $insertData = mysqli_query($this->status, $query);
+
+        if ($insertData) {
+            echo "<script>alert('Data inserted successfully!')</script>";
+        } else {
+            echo "<script>alert('Failed to insert data: " . mysqli_error($this->status) . "')</script>";
+        }
+    }
+
+    function selectDatabase()
+    {
+        $query = "SELECT * FROM product";
+
+        $selectData = mysqli_query($this->status, $query);
+
+        return $selectData;
+    }
+    function updateDatabase($id,$name, $age, $phone){
+        $query = "UPDATE product SET name = '$name', age = '$age', phone =$phone WHERE id=$id";
+        $updateData = mysqli_query($this->status, $query);
+        return $updateData;
+    }
+
+    function deleteDatabase($id){
+        $query = "DELETE FROM product WHERE id=$id";
+        $deleteData = mysqli_query($this->status, $query);
+        return $deleteData;
+    }
+}
